1f3ab90599d3e7323f63aeb70fc495c3e0575d1f
wesdresd11 <misterbizness777@tutamail.com>
2025-11-27 15:56:31 +0300

    Add files via upload

--- result.py
+++ result.py
@@ -1,11 +1,13 @@
 import circle
 import fahrenheit
 import is_even
+import palindrom
 
 def main():
     circle.main()
     fahrenheit.main()
     is_even.main()
+    palindrom.main()
 
 if __name__ == "__main__":
     main()
